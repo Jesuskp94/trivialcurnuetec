aea64dcdd74826e4f3e90d9ce46efd0492b13ca2
Mario0903 <mariolozano.0903@gmail.com>
2023-09-08 19:11:24 +0200

    api y clase Pregunta - 1911
    Creada la api y la clase Pregunta

---
+++ lib/ApiPreguntas.dart
@@ -0,0 +1,19 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+Future<List<Pregunta>> getPreguntas() async {
+  var url = Uri.https('opentdb.com', '/api.php?amount=10&type=multiple');
+  var response = await http.get(url);
+
+  if(response.statusCode == 200){
+    var jsonResponse = jsonDecode(response.body);
+
+    List<Pregunta> preguntas = [];
+    for(var pregunta in jsonResponse){
+      preguntas.add(Pregunta.fromJson(pregunta));
+    }
+    return preguntas;
+  }else{
+    throw Exception('La peticion ha fallado UwU srry');
+  }
+}
---
+++ lib/Pregunta.dart
@@ -0,0 +1,23 @@
+class Pregunta {
+  String categoria;
+  String frasePregunta;
+  String respCorrecta;
+  List<String> respIncorrecta;
+
+
+  Pregunta({
+    required this.categoria,
+    required this.frasePregunta,
+    required this.respCorrecta,
+    required this.respIncorrecta,
+  });
+
+  factory Pregunta.fromJson(Map<String, dynamic> json){
+    return Pregunta(
+      categoria : json['category'],
+      frasePregunta : json['question'],
+      respCorrecta : json['correct_answer'],
+      respIncorrecta : json['incorrect_answer'],
+    );
+  }
+}
