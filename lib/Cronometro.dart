9a41fe0d31e6fb2286283209acefa8880bf7678f
ELRACING <millan.delpon@salesianoslosboscos.com>
2023-09-08 20:49:06 +0200

    Cronometro FInalizado
    cronometro arreglado

---
+++ lib/Cronometro.dart
@@ -0,0 +1,57 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage();
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int tiempo = 0;
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        tiempo++;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Tiempo: $tiempo'),
+    );
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel(); // Asegúrate de cancelar el temporizador cuando el widget se dispose.
+    super.dispose();
+  }
+}
