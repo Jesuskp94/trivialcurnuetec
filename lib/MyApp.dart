6431ca5663c0fbfd2cff38a2391bd8d90c88d8ef
El Miwo <miguel.sastre@salesianoslosboscos.com>
2023-09-08 19:44:28 +0200

    Update MyApp.dart

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -1,32 +1,51 @@
 import 'package:flutter/material.dart';
 
+void main() { runApp(MyApp()); }
+
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _HomeScreenState();
+  State<MyApp> createState() => _MyAppState();
 }
 
-class _HomeScreenState extends State<MyApp> {
+class _MyAppState extends State<MyApp> {
   int puntos = 0; // Inicializa los puntos en 0
+
+  void sumarPuntos(int cantidad) {
+    setState(() {
+      puntos += cantidad; // Suma la cantidad de puntos ganados
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Trivial'),
-      ),
-      body: Center (
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Puntos: $puntos', // Muestra la cantidad de puntos
-              style: TextStyle(fontSize: 24),
-            ),
-          ],
+    return MaterialApp(
+      title: 'Trivial newtec',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Trivial'),
+        ),
+        body: Center (
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Puntos: $puntos', // Muestra la cantidad de puntos
+                style: TextStyle(fontSize: 24),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Simula ganar puntos al presionar un botón
+                  sumarPuntos(10); // Puedes cambiar la cantidad de puntos ganados
+                },
+                child: Text('Ganar puntos'),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
